25dec3e8c200ca27854a32870cc356fd7b496aa6
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-08-13 13:07:44 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,16 @@ output "ec2_public_dns" {
 output "ec2_private_ip" {
     value = aws_instance.my_instance.private_ip
 }
+
+# for count meta argument
+#output "ec2_public_ip" {
+ #   value = aws_instance.my_instance[*].public_ip
+#}
+
+#output "ec2_public_dns" {
+ #   value = aws_instance.my_instance[*].public_dns
+#}
+
+#output "ec2_private_ip" {
+ #   value = aws_instance.my_instance[*].private_ip
+#}
